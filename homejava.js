48fe9d9eeed4b1370506cce8825e66d68b87e2ca
Emma Paris <jamiefarrant707@gmail.com>
2021-08-17 10:26:20 +1200

    fixed JS for start location to be adaptable

--- homejava.js
+++ homejava.js
@@ -1,34 +1,16 @@
-places = ["null", "Leeston", "Southbridge", "Rolleston", "Lincoln", "Christchurch"]
-destinations = ["null", "Leeston", "Southbridge", "Rolleston", "Lincoln", "Christchurch"]
-startLocationSelected = 0;
-destinationLocationSelected = 0;
+var places = ["null", "Leeston", "Southbridge", "Rolleston", "Lincoln", "Christchurch", " "]
+var destinations = ["null", "Leeston", "Southbridge", "Rolleston", "Lincoln", "Christchurch"]
 var customWindow = window.open('', '_blank', '');
     customWindow.close();;
 
 function addLocation_click(){
-
-    if (startLocationSelected == 0){
     let startLocation = document.getElementById("startlocation").value;
 
-    document.getElementById("startlocationresult").innerHTML += places[startLocation];
-    }
-    startLocationSelected = 1
+    document.getElementById("startlocationresult").innerHTML = "Starting place:"+places[startLocation];
 }
-
 function addDestination_click(){
 
-    if (destinationLocationSelected == 0){
     let destinationLocation = document.getElementById("destination").value;
 
     document.getElementById("destinationList").innerHTML += " " +[destinationLocation] + ", ";
-
-    }
-    destinationLocationSelected = 0
 }
-
-function resetStart_click(){
-
-    let text = document.getElementById("startlocationresult").innerHTML += places[startLocation];
-    text.replace(places[startLocation], " ");
-}
-
